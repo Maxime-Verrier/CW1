3284aab54cfdfe4cfc03a6394194ebcd813e0c64
Maxime-Verrier <57402477+Maxime-Verrier@users.noreply.github.com>
2023-02-23 21:40:10 +0000

    [Refractor] Proto camera to basic camera with mouse movement and max/min angle

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -2,16 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject target;
-    public float pLerp = .02f;
-    public float rLerp = .01f;
-    public Vector3 offset = Vector3.zero;
+    private Vector2 angle = new Vector2(45f, 0f);
 
-    void Update()
+    [SerializeField]
+    public Transform target = default;
+
+    [SerializeField]
+    public float pLerp = .02f;
+
+    [SerializeField]
+    public float rLerp = .01f;
+
+    [SerializeField]
+    public float offset = 0;
+
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
+    void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, pLerp);
-        transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, rLerp);
+        Quaternion lookRotation;
+
+        if (ManualRotation())
+        {
+            resetAngles();
+            lookRotation = Quaternion.Euler(angle);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+        Vector3 focusPoint = target.position;
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = Vector3.Lerp(transform.position, focusPoint - lookDirection * offset, pLerp);
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    bool ManualRotation()
+    {
+        Vector2 input = new Vector2(
+            Input.GetAxis("Mouse Y"),
+            Input.GetAxis("Mouse X")
+        );
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            angle += rotationSpeed * Time.unscaledDeltaTime * input;
+            return true;
+        }
+        return false;
+    }
+    void resetAngles()
+    {
+        angle.x = Mathf.Clamp(angle.x, minVerticalAngle, maxVerticalAngle);
+
+        if (angle.y < 0f)
+        {
+            angle.y += 360f;
+        }
+        else if (angle.y >= 360f)
+        {
+            angle.y -= 360f;
+        }
     }
 }
