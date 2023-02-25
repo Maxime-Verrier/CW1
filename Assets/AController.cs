7a34a9901f872a81c4332f237bbff3647691a9df
Maxime-Verrier <57402477+Maxime-Verrier@users.noreply.github.com>
2023-02-25 09:39:42 +0000

    [Add] base for camera interactable

--- Assets/AController.cs
+++ Assets/AController.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AController : MonoBehaviour
+public abstract class AController : MonoBehaviour, IInteractable
 {
+    [SerializeField] public float interactRange = 15;
+
+    public float Range => interactRange;
+
     public abstract void onDisabled();
+
+    public void OnEndHover()
+    {
+    }
+
+    public void OnInteract(Player player)
+    {
+        player.SwapController(this);
+    }
+
+    public void OnStartHover()
+    {
+    }
 }
