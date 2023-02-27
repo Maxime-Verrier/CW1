c55f3e723042f242828f84db9a4d4727e77ad559
Maxime-Verrier <57402477+Maxime-Verrier@users.noreply.github.com>
2023-02-27 18:26:33 +0000

    [Add] Base Level 1, First Part of LvL2, Elevator

---
+++ Assets/Interaction/Elevator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour, IInteractor
+{
+    [SerializeField] private Vector3[] targets;
+    [SerializeField] private float speed = 1;
+
+    private int targetIndex = 0;
+    private bool moving = false;
+
+        void Update()
+    {
+        if (moving)
+        {
+            if (Vector3.Distance(transform.position, targets[targetIndex]) < 0.01f)
+            {
+                targetIndex = targetIndex >= targets.Length - 1 ? 0 : targetIndex + 1;
+                moving = false;
+            }
+
+            else transform.position = Vector3.MoveTowards(transform.position, targets[targetIndex], speed * Time.deltaTime);
+        }
+    }
+
+    public void OnInteract(Player player)
+    {
+        moving = !moving;
+    }
+}
